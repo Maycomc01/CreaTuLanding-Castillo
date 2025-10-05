0a189cbced85d9b02b94ad6800a897a8a9c0d39c
mayco-castillo <maycoangelocastillo@gmail.com>
2025-10-05 01:05:44 -0300

    primeros avances

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,16 @@
+import "./NavBar.css";
+const NavBar = () => {
+  return (
+    <nav className="navbar">
+      <h1 className="logo">Juanita de las nieves</h1>
+      <ul className="nav-links">
+        <li>inicio</li>
+        <li>Librería</li>
+        <li>Marroquinería</li>
+        <li>Oficina</li>
+        <li>Combos</li>
+      </ul>
+    </nav>
+  );
+};
+export default NavBar;
