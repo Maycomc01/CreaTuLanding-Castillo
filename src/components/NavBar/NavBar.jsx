1f8f8dda8e418d47a932d435a9cbdbfe80e5e66e
mayco-castillo <maycoangelocastillo@gmail.com>
2025-11-19 19:47:33 -0300

    a

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,7 +6,7 @@ const NavBar = () => {
     <nav className={styles.navbar}>
       <header className={styles.presentacion}>
         <img className={styles.img} src="../src/img/logo.png" alt="logo" />
-        <h1 className={styles.nombre}>Juanita</h1>
+        <h1 className={styles.nombre}>Juanita de las Nieves</h1>
       </header>
       <ul className={styles.navLinks}>
         <NavLink to="/">Inicio</NavLink>
